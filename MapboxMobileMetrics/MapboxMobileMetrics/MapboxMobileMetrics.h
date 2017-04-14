ebbb371808f19f24febb129c41d5c1bee0503104
Jesse Bounds <jesse@rebounds.net>
2017-04-14 15:43:49 -0700

    Initial objc test

---
+++ MapboxMobileMetrics/MapboxMobileMetrics/MapboxMobileMetrics.h
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapboxMobileMetrics.
+FOUNDATION_EXPORT double MapboxMobileMetricsVersionNumber;
+
+//! Project version string for MapboxMobileMetrics.
+FOUNDATION_EXPORT const unsigned char MapboxMobileMetricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapboxMobileMetrics/PublicHeader.h>
+
+@interface MapboxMobileMetrics : NSObject
+
+- (NSString *)sayHelloTo:(NSString *)name;
+
+@end
+
+
---
+++ MapboxMobileMetrics/MapboxMobileMetrics/MapboxMobileMetrics.m
@@ -0,0 +1,9 @@
+#import "MapboxMobileMetrics.h"
+
+@implementation MapboxMobileMetrics
+
+- (NSString *)sayHelloTo:(NSString *)name {
+    return [NSString stringWithFormat:@"hello %@", name];
+}
+
+@end
