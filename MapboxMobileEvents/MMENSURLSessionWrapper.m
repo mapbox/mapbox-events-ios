3560f850aa3b1d2d886312135dab9557e0870c28
Jesse Bounds <jesse@rebounds.net>
2017-07-12 10:58:00 -0700

    Update TrustKit config comments for clarity

--- MapboxMobileEvents/MMENSURLSessionWrapper.m
+++ MapboxMobileEvents/MMENSURLSessionWrapper.m
@@ -32,11 +32,11 @@
     @{
       kTSKSwizzleNetworkDelegates: @NO,
       kTSKPinnedDomains: @{
+              /* Production */
               @"events.mapbox.com" : @{
                       kTSKEnforcePinning:@YES,
                       kTSKPublicKeyAlgorithms : @[kTSKAlgorithmRsa2048],
                       kTSKPublicKeyHashes : @[
-                              /* Production */
                               // Digicert, 2016, SHA1 Fingerprint=0A:80:27:6E:1C:A6:5D:ED:1D:C2:24:E7:7D:0C:A7:24:0B:51:C8:54
                               @"Tb0uHZ/KQjWh8N9+CZFLc4zx36LONQ55l6laDi1qtT4=",
                               // Digicert, 2017, SHA1 Fingerprint=E2:8E:94:45:E0:B7:2F:28:62:D3:82:70:1F:C9:62:17:F2:9D:78:68
@@ -47,11 +47,11 @@
                               @"yJLOJQLNTPNSOh3Btyg9UA1icIoZZssWzG0UmVEJFfA=",
                               ]
                       },
+              /* Staging */
               @"*.tilestream.net" : @{
                       kTSKEnforcePinning:@YES,
                       kTSKPublicKeyAlgorithms : @[kTSKAlgorithmRsa2048],
                       kTSKPublicKeyHashes : @[
-                              /* Staging */
                               // Digicert, SHA1 Fingerprint=C6:1B:FE:8C:59:8F:29:F0:36:2E:88:BB:A2:CD:08:3B:F6:59:08:22
                               @"3euxrJOrEZI15R4104UsiAkDqe007EPyZ6eTL/XxdAY=",
                               // Stub: TrustKit requires 2 hashes for every endpoint
