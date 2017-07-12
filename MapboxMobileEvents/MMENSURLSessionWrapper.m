44a554799732c93f049887abcd4b826645a2ef80
Jesse Bounds <jesse@rebounds.net>
2017-07-11 18:00:20 -0700

    Add staging cert pin hashes

--- MapboxMobileEvents/MMENSURLSessionWrapper.m
+++ MapboxMobileEvents/MMENSURLSessionWrapper.m
@@ -36,6 +36,7 @@
                       kTSKEnforcePinning:@YES,
                       kTSKPublicKeyAlgorithms : @[kTSKAlgorithmRsa2048],
                       kTSKPublicKeyHashes : @[
+                              /* Production */
                               // Digicert, 2016, SHA1 Fingerprint=0A:80:27:6E:1C:A6:5D:ED:1D:C2:24:E7:7D:0C:A7:24:0B:51:C8:54
                               @"Tb0uHZ/KQjWh8N9+CZFLc4zx36LONQ55l6laDi1qtT4=",
                               // Digicert, 2017, SHA1 Fingerprint=E2:8E:94:45:E0:B7:2F:28:62:D3:82:70:1F:C9:62:17:F2:9D:78:68
@@ -45,7 +46,20 @@
                               // Geotrust, 2017, SHA1 Fingerprint=20:CE:AB:72:3C:51:08:B2:8A:AA:AB:B9:EE:9A:9B:E8:FD:C5:7C:F6
                               @"yJLOJQLNTPNSOh3Btyg9UA1icIoZZssWzG0UmVEJFfA=",
                               ]
-                      }}};
+                      },
+              @"*.tilestream.net" : @{
+                      kTSKEnforcePinning:@YES,
+                      kTSKPublicKeyAlgorithms : @[kTSKAlgorithmRsa2048],
+                      kTSKPublicKeyHashes : @[
+                              /* Staging */
+                              // Digicert, SHA1 Fingerprint=C6:1B:FE:8C:59:8F:29:F0:36:2E:88:BB:A2:CD:08:3B:F6:59:08:22
+                              @"3euxrJOrEZI15R4104UsiAkDqe007EPyZ6eTL/XxdAY=",
+                              // Stub: TrustKit requires 2 hashes for every endpoint
+                              @"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+                              ]
+                      }
+              }
+      };
     [TrustKit initializeWithConfiguration:trustKitConfig];
 }
 
