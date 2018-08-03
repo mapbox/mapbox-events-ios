8b71ed214ebb09cce5e16f89702905673da2efc1
Randall C Lee <randallee@gmail.com>
2018-08-03 17:31:29 -0400

    Update TrustKit to use HashProvider
    Injects MMEEventsConfiguration in order to gain access to blacklist.

--- MapboxMobileEvents/MMETrustKitProvider.h
+++ MapboxMobileEvents/MMETrustKitProvider.h
@@ -1,9 +1,10 @@
 #import <Foundation/Foundation.h>
+#import "MMEEventsConfiguration.h"
 
 @class TrustKit;
 
 @interface MMETrustKitProvider : NSObject
 
-+ (TrustKit *)trustKitWithUpdatedConfiguration;
++ (TrustKit *)trustKitWithUpdatedConfiguration:(MMEEventsConfiguration *)configuration;
 
 @end
