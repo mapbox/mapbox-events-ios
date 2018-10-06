6aac093fbdbfb4210c258675e1792f8881922966
Randall C Lee <randallee@gmail.com>
2018-10-06 13:52:00 -0400

    Add MMEMetricsManager

---
+++ MapboxMobileEvents/MMEMetricsManager.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MMEMetricsManager : NSObject
+
++ (instancetype)sharedManager;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MapboxMobileEvents/MMEMetricsManager.m
@@ -0,0 +1,38 @@
+#import "MMEMetricsManager.h"
+
+@interface MMEMetricsManager ()
+
+@property (nonatomic) int requests;
+@property (nonatomic) int totalDataTransfer;
+@property (nonatomic) int cellDataTransfer;
+@property (nonatomic) int wifiDataTransfer;
+@property (nonatomic) int appWakeups;
+@property (nonatomic) int eventCountFailed;
+@property (nonatomic) int eventCountTotal;
+@property (nonatomic) int eventCountMax;
+@property (nonatomic) int deviceTimeDrift;
+@property (nonatomic) float deviceLat;
+@property (nonatomic) float deviceLon;
+@property (nonatomic) NSDate *dateUTC;
+@property (nonatomic) NSDictionary *configResponseDict;
+@property (nonatomic) NSMutableDictionary *eventCountPerType;
+@property (nonatomic) NSMutableDictionary *failedRequestsDict;
+
+@end
+
+@implementation MMEMetricsManager
+
++ (instancetype)sharedManager {
+    static MMEMetricsManager *_sharedManager;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[MMEMetricsManager alloc] init];
+    });
+    
+    return _sharedManager;
+}
+
+
+
+@end
