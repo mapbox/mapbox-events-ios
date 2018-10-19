175866a0748703dd068c479c8397ce95caba1f29
Randall C Lee <randallee@gmail.com>
2018-10-19 10:07:16 -0400

    Use dot notation

--- MapboxMobileEvents/MMEMetricsManager.m
+++ MapboxMobileEvents/MMEMetricsManager.m
@@ -61,7 +61,7 @@
         
         if ([error.userInfo objectForKey:MMEResponseKey]) {
             NSHTTPURLResponse *response = (NSHTTPURLResponse *)[error.userInfo objectForKey:MMEResponseKey];
-            NSString *urlString = [[response URL] absoluteString];
+            NSString *urlString = response.URL.absoluteString;
             NSNumber *statusCode = @(response.statusCode);
             NSString *statusCodeString = [statusCode stringValue];
             NSString *failedRequestKey = [NSString stringWithFormat:@"%@, %@",urlString, statusCodeString];
