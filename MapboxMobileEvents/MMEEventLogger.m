f3cbe3bd6c565b5ce3b2e505564127cc19efa303
Randall C Lee <randallee@gmail.com>
2020-01-02 11:39:03 -0800

    Remove remaining debug code

--- MapboxMobileEvents/MMEEventLogger.m
+++ MapboxMobileEvents/MMEEventLogger.m
@@ -1,17 +1,7 @@
 #import "MMEEventLogger.h"
 #import "MMEEvent.h"
-#import "MMEUINavigation.h"
 #import "MMEDate.h"
 #import <WebKit/WebKit.h>
-
-@interface MMEEventLogger()
-
-@property (nonatomic, copy) NSString *dateForDebugLogFile;
-@property (nonatomic) dispatch_queue_t debugLogSerialQueue;
-@property (nonatomic) NSDate *nextLogFileDate;
-@property (nonatomic, getter=isTimeForNewLogFile) BOOL timeForNewLogFile;
-
-@end
 
 @implementation MMEEventLogger
 
@@ -26,28 +16,7 @@
     return _sharedLogger;
 }
 
-+ (NSDateFormatter *)logFileDateFormatter {
-    static NSDateFormatter *_logFileFormatter = nil;
-    if (!_logFileFormatter) {
-        _logFileFormatter = [[NSDateFormatter alloc] init];
-        [_logFileFormatter setDateFormat:@"yyyy'-'MM'-'dd"];
-        [_logFileFormatter setTimeZone:[NSTimeZone systemTimeZone]];
-    }
-
-    return _logFileFormatter;
-}
-
 #pragma mark -
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        MMEDate* now = [MMEDate date];
-        self.dateForDebugLogFile = [MMEEventLogger.logFileDateFormatter stringFromDate:now];
-        self.nextLogFileDate = [now mme_startOfTomorrow];
-    }
-    return self;
-}
 
 - (void)logEvent:(MMEEvent *)event {
     if (self.isEnabled) {
