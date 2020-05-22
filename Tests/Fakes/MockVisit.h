89e1cd601022ea038536dabfd27da68af9fa5f7e
Dane Miluski <dmiluski@gmail.com>
2020-05-22 15:57:19 -0700

    [CI] Carthage CI Fail
    Problem:
    Bitrise Carthage step fails if it is run with an empty resolved file.
    
    Change:
    Retain this resolve file contents until the carthage dependencies are merged to master.

--- Tests/Fakes/MockVisit.h
+++ Tests/Fakes/MockVisit.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 -(instancetype)init NS_UNAVAILABLE;
 
+
 -(instancetype)initWithArrivalDate:(NSDate*)arrivalDate
                      departureDate:(NSDate*)departureDate
                         coordinate:(CLLocationCoordinate2D)coordinate
