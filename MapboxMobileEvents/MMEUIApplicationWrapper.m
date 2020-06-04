6731abecf69fb0dd7520a0c62d7cba3ff9b818d6
Dane Miluski <dmiluski@gmail.com>
2020-06-04 14:01:27 -0700

    [MMEUIApplicationWrapper] Tests
    Goal:
    Boost class coverage to 100%
    
    Approach:
    Rather than directly accessing UIApplication.shared, provide mechanism to inject dependency allowing to test this classes responsibilities.
    
    To achieve this, pass in a MMEUIApplicationWrapper, the interface that is captured by this class and the UIApplication. By passing this in, we can ensure our wrapper is making the correct calls to the dependency UIApplication.
    
    Change:
    + Add Docs
    + Add dependency property
    + Add Designated/Default initializers
    + Add Tests to verify calls/responses

--- MapboxMobileEvents/MMEUIApplicationWrapper.m
+++ MapboxMobileEvents/MMEUIApplicationWrapper.m
@@ -1,17 +1,38 @@
 #import "MMEUIApplicationWrapper.h"
 
+@interface MMEUIApplicationWrapper ()
+@property (nonatomic, strong) id <MMEUIApplicationWrapper> application;
+@end
+
 @implementation MMEUIApplicationWrapper
 
-- (UIApplicationState)applicationState {
-    return [UIApplication sharedApplication].applicationState;
+// MARK: - Initializers
+
+- (instancetype)init {
+    return [self initWithApplication:UIApplication.sharedApplication];
 }
 
+- (instancetype)initWithApplication:(id<MMEUIApplicationWrapper>)application {
+    if (self = [super init]) {
+        self.application = application;
+    }
+    return self;
+}
+
+// MARK: - Properties
+
+- (UIApplicationState)applicationState {
+    return self.application.applicationState;
+}
+
+// MARK: - Methods
+
 - (UIBackgroundTaskIdentifier)beginBackgroundTaskWithExpirationHandler:(void(^ __nullable)(void))handler {
-    return [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:handler];
+    return [self.application beginBackgroundTaskWithExpirationHandler:handler];
 }
 
 - (void)endBackgroundTask:(UIBackgroundTaskIdentifier)identifier {
-    [[UIApplication sharedApplication] endBackgroundTask:identifier];
+    [self.application endBackgroundTask:identifier];
 }
 
 @end
---
+++ Tests/MMEUIApplicationWrapperTests.m
@@ -0,0 +1,75 @@
+#import <XCTest/XCTest.h>
+#import "MMEUIApplicationWrapper.h"
+
+// MARK: - Mock Class
+@interface MockApplicationWrapper: NSObject <MMEUIApplicationWrapper>
+
+@property (nonatomic, assign) NSUInteger applicationStateCallCount;
+@property (nonatomic, assign) NSUInteger beginBackgroundCallCount;
+@property (nonatomic, assign) NSUInteger endBackgroundTaskCallCount;
+
+
+@end
+
+@implementation MockApplicationWrapper
+
+- (UIApplicationState)applicationState {
+    self.applicationStateCallCount += 1;
+    return UIApplicationStateBackground;
+}
+
+- (UIBackgroundTaskIdentifier)beginBackgroundTaskWithExpirationHandler:(void (^)(void))handler {
+    self.beginBackgroundCallCount += 1;
+    return 42;
+}
+
+- (void)endBackgroundTask:(UIBackgroundTaskIdentifier)identifier {
+    self.endBackgroundTaskCallCount += 1;
+}
+
+@end
+
+// MARK: - Tests
+@interface MMEUIApplicationWrapper (Private)
+@property (nonatomic, strong) id <MMEUIApplicationWrapper> application;
+@end
+
+@interface MMEUIApplicationWrapperTests : XCTestCase
+@property (nonatomic, strong) MockApplicationWrapper* mock;
+@property (nonatomic, strong) MMEUIApplicationWrapper* application;
+@end
+
+@implementation MMEUIApplicationWrapperTests
+
+- (void)setUp {
+    self.mock = [[MockApplicationWrapper alloc] init];
+    self.application = [[MMEUIApplicationWrapper alloc] initWithApplication:self.mock];
+}
+
+- (void)testDefaultInitializer {
+    // Expect Wrapper to have a reference to shared application
+    id <MMEUIApplicationWrapper> application = UIApplication.sharedApplication;
+    XCTAssertEqual(MMEUIApplicationWrapper.new.application, application);
+}
+- (void)testDesignatedInitializer {
+    MockApplicationWrapper *mock = [[MockApplicationWrapper alloc] init];
+    MMEUIApplicationWrapper *wrapper = [[MMEUIApplicationWrapper alloc] initWithApplication:mock];
+    XCTAssertEqual(wrapper.application, mock);
+}
+
+- (void)testApplicationState {
+    XCTAssertEqual(self.application.applicationState, UIApplicationStateBackground);
+    XCTAssertEqual(self.mock.applicationStateCallCount, 1);
+}
+
+- (void)testBeginBackgroundTask {
+    XCTAssertEqual([self.application beginBackgroundTaskWithExpirationHandler:^{}], 42);
+    XCTAssertEqual(self.mock.beginBackgroundCallCount, 1);
+}
+
+- (void)testEndBackgroundTask {
+    [self.application endBackgroundTask:42];
+    XCTAssertEqual(self.mock.endBackgroundTaskCallCount, 1);
+}
+
+@end
