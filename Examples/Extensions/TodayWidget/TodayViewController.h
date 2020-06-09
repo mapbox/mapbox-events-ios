b4e4cccce3237b34e05c38a51e5d34c6916f2115
Dane Miluski <dmiluski@gmail.com>
2020-06-09 13:38:41 -0700

    Extension Example: Today Widget
    Goal:
    Show that MapboxMobileEvents SDK is able to be built/run in an extension.
    
    Approach:
    Provide a Today Widget example loading MapboxMobileEvents SDK and displaying (demonstrated on iPhone SE)
    
    Outcome:
    Able to build/run without crashing, although there is a warning due to the use of UIApplication.shared application directly inside of the framework.
    
    > d: warning: linking against a dylib which is not safe for use in application extensions: /Users/danemiluski/Library/Developer/Xcode/DerivedData/MapboxMobileEvents-gtwngrjytwknvkfqciqcsoxcqjvr/Build/Products/Debug-iphonesimulator/MapboxMobileEvents.framework/MapboxMobileEvents
    
    This warning could be remedied by enabling Allow app extension API only. Unfortunately, this would alos prevent the use of UIApplication.shared which can prove useful to identify UIApplication’s state.

---
+++ Examples/Extensions/TodayWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TodayWidget
+//
+//  Created by Dane Miluski on 6/9/20.
+//  Copyright © 2020 Mapbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
