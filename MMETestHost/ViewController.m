6ad2b8f976c405d87f1140cc7848bc04a28f36c8
Dane Miluski <dmiluski@gmail.com>
2020-06-10 17:02:57 -0700

    [User Defaults Sharing] Sharing UserDefaults between App/Extension
    Goal:
    Experiment with UserDefault sharing between App/Extension
    
    Outcome:
    Through experimentation, confirmed what I wanted to clarify with the team on behaviors.
    
    I access a shared user default between App/Extensions a UserDefaults(suite:) needs to be instantiated with the shared AppGroup provided from the top level application which has been configured with App Group.
    
    If the goal is to support sharing user default values between App/Extension we would need to request our developers set the shared app group and we leverage it to access the shared resource.
    
    Experiment:
    Setup App Group and hop between App/Extension reading/writing to see what is visible between the two.
    
    Manual Tests:
    Evaluated the behavior of the App and extension by reading and writing. Launching each one after the other to verify whether the other could see the data written by the other
    
    - Mismatch suite to App Group (No data sharing)
    - Matchin suite with AppGroup (Data can be read/write between the two)

---
+++ MMETestHost/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  MapboxMobileEvents
+//
+//  Created by Dane Miluski on 6/10/20.
+//  Copyright © 2020 Mapbox. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
