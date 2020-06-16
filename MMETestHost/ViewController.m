ff9819c20e45be4b7fd47813970f520ba207c532
Dane Miluski <dmiluski@gmail.com>
2020-06-16 10:44:07 -0700

    PR Feedback:
    - Remove commented out code
    - Remove commented out/unused property
    - Remove unneeded comments
    - Remove header doc

--- MMETestHost/ViewController.m
+++ MMETestHost/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  MapboxMobileEvents
-//
-//  Created by Dane Miluski on 6/10/20.
-//  Copyright © 2020 Mapbox. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,17 +8,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
