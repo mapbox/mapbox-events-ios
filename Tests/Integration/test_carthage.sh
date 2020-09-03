72e33e2c52c427eae6a9c4f002349a01b12640ad
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2020-09-03 16:17:25 +0200

    Run integration tests on CI

--- Tests/Integration/test_carthage.sh
+++ Tests/Integration/test_carthage.sh
@@ -5,6 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="${DIR}/../.."
 pushd "${ROOT_DIR}/Tests/Integration/Carthage"
 
+if [ -z `which xcodegen` ]; then
+     brew install xcodegen
+fi
+
 sed -i '' -e "s|@PATH@|file:///${ROOT_DIR}|g" "${ROOT_DIR}/Tests/Integration/Carthage/Cartfile"
 xcodegen generate
 carthage update --platform iOS --use-netrc
