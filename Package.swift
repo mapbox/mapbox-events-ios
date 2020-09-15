bd78e9f64b077e19079730834c61825689ab32b9
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2020-09-15 13:20:03 +0200

    Lower SPM deployment target to iOS 9

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MapboxMobileEvents",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v9)
     ],
     products: [
         .library(
