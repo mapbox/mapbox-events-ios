f2454b77327ac6b9cb9eaf4ea002d6c3b1cd9d6b
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2020-10-22 15:10:58 +0200

    Restructure sources to fix Xcode SPM integration
    When integrating an SPM package with Xcode, SWIFT_PACKAGE
    is undefined. This causes the umbrella header to behave
    incorrectly. By folding MME sources into
    Sources/MapboxMobileEvents/include we can use the same umbrella
    for CocoaPods, SPM, Carthage.

--- Sources/MapboxMobileEvents/include/MapboxMobileEvents.h
+++ Sources/MapboxMobileEvents/include/MapboxMobileEvents.h
@@ -7,16 +7,8 @@ FOUNDATION_EXPORT double MapboxMobileEventsVersionNumber;
 FOUNDATION_EXPORT const unsigned char MapboxMobileEventsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MapboxMobileEvents/PublicHeader.h>
-#if SWIFT_PACKAGE
-#import "../MMEConstants.h"
-#import "../MMEEvent.h"
-#import "../MMETypes.h"
-#import "../MMEEventsManager.h"
-#import "../NSUserDefaults+MMEConfiguration.h"
-#else
 #import <MapboxMobileEvents/MMEConstants.h>
 #import <MapboxMobileEvents/MMEEvent.h>
 #import <MapboxMobileEvents/MMETypes.h>
 #import <MapboxMobileEvents/MMEEventsManager.h>
 #import <MapboxMobileEvents/NSUserDefaults+MMEConfiguration.h>
-#endif
