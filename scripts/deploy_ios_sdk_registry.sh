6448e8006a934bb9bc53a46a39b0c8c83b628881
Vadim Yeshkunov <vadim.yeshkunov@mapbox.com>
2021-05-18 08:03:24 +0300

    [build] Build artifacts for SDK registry

---
+++ scripts/deploy_ios_sdk_registry.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# This script deploys MapboxMobileEvents SDK for iOS to SDK Registry
+# <artifacts_dir>/MapboxMobileEvents.xcframework.zip (archive containing MapboxMobileEvents.xcframework)
+# <artifacts_dir>/MapboxMobileEvents.zip (archive containing MapboxMobileEvents.framework)
+
+set -eo pipefail
+
+if [ "$IOS_DEPLOY_DRYRUN" != "false" ]; then
+    DRYRUN=--dryrun
+else
+    DRYRUN=
+fi
+
+if [[ $# -lt 2 ]] ; then
+    echo 'Script expects two arguments'
+    echo 'Usage: deploy_ios_sdk_registry.sh <tag> <artifacts_dir>'
+    exit 1
+fi
+
+VERSION_TAG=$1
+VERSION=${VERSION_TAG#v}
+
+ARTIFACTS_DIR="$2"
+
+ZIP_XCFRAMEWORK_FILE="MapboxMobileEvents.xcframework.zip"
+ZIP_FRAMEWORK_FILE="MapboxMobileEvents.zip"
+
+ZIP_XCFRAMEWORK="${ARTIFACTS_DIR}/${ZIP_XCFRAMEWORK_FILE}"
+ZIP_FRAMEWORK="${ARTIFACTS_DIR}/${ZIP_FRAMEWORK_FILE}"
+
+if [ ! -f "${ZIP_XCFRAMEWORK}" ]; then
+    echo "${ZIP_XCFRAMEWORK} not found"
+    exit 1
+fi
+
+if [ ! -f "${ZIP_FRAMEWORK}" ]; then
+    echo "${ZIP_FRAMEWORK} not found"
+    exit 1
+fi
+
+if [ -z `which aws` ]; then
+    brew install awscli
+fi
+
+echo "Deploying…"
+aws s3 cp ${DRYRUN} ${ZIP_XCFRAMEWORK} s3://mapbox-api-downloads-production/v2/mapbox-events-ios/releases/ios/${VERSION}/packages/${ZIP_XCFRAMEWORK_FILE}
+aws s3 cp ${DRYRUN} ${ZIP_FRAMEWORK} s3://mapbox-api-downloads-production/v2/mapbox-events-ios/releases/ios/${VERSION}/packages/${ZIP_FRAMEWORK_FILE}
---
+++ scripts/prepare_release_artifacts.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+# This script creates MapboxMobileEvents artifacts
+
+set -eo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="${DIR}/.."
+
+echo ${ROOT_DIR}
+
+# cleanup artifacts folder
+rm -rf $ROOT_DIR/build/artifacts
+
+# build iOS .frameworks
+xcodebuild archive \
+        -archivePath ${ROOT_DIR}/build/artifacts/frameworks/iOS \
+        -project ${ROOT_DIR}/MapboxMobileEvents.xcodeproj \
+        -scheme MapboxMobileEvents \
+        -destination="iOS" \
+        -sdk iphoneos \
+        -UseModernBuildSystem=YES \
+        -derivedDataPath ${ROOT_DIR}/build/derivedDataPath/iOS \
+        ONLY_ACTIVE_ARCH=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+        SKIP_INSTALL=NO \
+        SUPPORTS_MACCATALYST=NO \
+        -configuration Release
+
+# build Catalyst .framework
+xcodebuild archive \
+        -archivePath ${ROOT_DIR}/build/artifacts/frameworks/iOS-Catalyst \
+        -project ${ROOT_DIR}/MapboxMobileEvents.xcodeproj \
+        -scheme MapboxMobileEvents \
+        -destination="iOS" \
+        -UseModernBuildSystem=YES \
+        -derivedDataPath ${ROOT_DIR}/build/derivedDataPath/iOS-Catalyst \
+        ONLY_ACTIVE_ARCH=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+        SKIP_INSTALL=NO \
+        SUPPORTS_MACCATALYST=YES \
+        -configuration Release
+
+# build iOS Simulator .framework
+xcodebuild archive \
+        -archivePath ${ROOT_DIR}/build/artifacts/frameworks/iOS-Simulator \
+        -project ${ROOT_DIR}/MapboxMobileEvents.xcodeproj \
+        -scheme MapboxMobileEvents \
+        -destination="iOS Simulator" \
+        -sdk iphonesimulator \
+        -UseModernBuildSystem=YES \
+        -derivedDataPath ${ROOT_DIR}/build/derivedDataPath/iOS-Simulator \
+        ONLY_ACTIVE_ARCH=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+        SKIP_INSTALL=NO \
+        SUPPORTS_MACCATALYST=YES \
+        -configuration Release
+
+# build .xcframework
+xcodebuild \
+    -create-xcframework \
+    -framework "${ROOT_DIR}/build/artifacts/frameworks/iOS.xcarchive/Products/Library/Frameworks/MapboxMobileEvents.framework" \
+    -framework "${ROOT_DIR}/build/artifacts/frameworks/iOS-Catalyst.xcarchive/Products/Library/Frameworks/MapboxMobileEvents.framework" \
+    -framework "${ROOT_DIR}/build/artifacts/frameworks/iOS-Simulator.xcarchive/Products/Library/Frameworks/MapboxMobileEvents.framework" \
+    -output "${ROOT_DIR}/build/artifacts/frameworks/MapboxMobileEvents.xcframework"
+
+# zip artifacts
+mkdir -p ${ROOT_DIR}/build/artifacts/zip
+ZIPDIR=${ROOT_DIR}/build/artifacts/zip
+
+pushd ${ROOT_DIR}/build/artifacts/frameworks/iOS.xcarchive/Products/Library/Frameworks
+zip -r $ZIPDIR/MapboxMobileEvents.zip MapboxMobileEvents.framework
+popd
+
+pushd ${ROOT_DIR}/build/artifacts/frameworks
+zip -r $ZIPDIR/MapboxMobileEvents.xcframework.zip MapboxMobileEvents.xcframework
+popd
