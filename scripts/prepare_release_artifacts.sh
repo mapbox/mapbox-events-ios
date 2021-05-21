4ff47377561eeb7a3502f1ca1a02dcafc442c980
Roman Gardukevich <roman.gardukevich@mapbox.com>
2021-05-21 14:45:33 +0300

    Support arm64 iOS Simulator, Catalyst and dSyms in XCFramework (#286)
    * Return arm64-simulator support
    
    * Preserve symlinks in zip archives
    
    * Add dSyms to XCFramework
    
    * Simplify xcodebuild call
    
    * Improve shell escaping
    
    * Build artifacts on Xcode 12.2 to include arm64-catalyst

--- scripts/prepare_release_artifacts.sh
+++ scripts/prepare_release_artifacts.sh
@@ -13,64 +13,50 @@ rm -rf $ROOT_DIR/build/artifacts
 
 # build iOS .frameworks
 xcodebuild archive \
-        -archivePath ${ROOT_DIR}/build/artifacts/frameworks/iOS \
-        -project ${ROOT_DIR}/MapboxMobileEvents.xcodeproj \
+        -archivePath "${ROOT_DIR}/build/artifacts/frameworks/iOS" \
+        -project "${ROOT_DIR}/MapboxMobileEvents.xcodeproj" \
         -scheme MapboxMobileEvents \
-        -destination="iOS" \
-        -sdk iphoneos \
-        -UseModernBuildSystem=YES \
-        -derivedDataPath ${ROOT_DIR}/build/derivedDataPath/iOS \
-        ONLY_ACTIVE_ARCH=NO \
+        -destination "generic/platform=iOS"\
         BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-        SKIP_INSTALL=NO \
-        SUPPORTS_MACCATALYST=NO \
-        -configuration Release
+        SKIP_INSTALL=NO
 
 # build Catalyst .framework
 xcodebuild archive \
-        -archivePath ${ROOT_DIR}/build/artifacts/frameworks/iOS-Catalyst \
-        -project ${ROOT_DIR}/MapboxMobileEvents.xcodeproj \
+        -archivePath "${ROOT_DIR}/build/artifacts/frameworks/iOS-Catalyst" \
+        -project "${ROOT_DIR}/MapboxMobileEvents.xcodeproj" \
         -scheme MapboxMobileEvents \
-        -destination="iOS" \
-        -UseModernBuildSystem=YES \
-        -derivedDataPath ${ROOT_DIR}/build/derivedDataPath/iOS-Catalyst \
-        ONLY_ACTIVE_ARCH=NO \
+        -destination "platform=macOS,variant=Mac Catalyst" \
         BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-        SKIP_INSTALL=NO \
-        SUPPORTS_MACCATALYST=YES \
-        -configuration Release
+        SKIP_INSTALL=NO
 
 # build iOS Simulator .framework
 xcodebuild archive \
-        -archivePath ${ROOT_DIR}/build/artifacts/frameworks/iOS-Simulator \
-        -project ${ROOT_DIR}/MapboxMobileEvents.xcodeproj \
+        -archivePath "${ROOT_DIR}/build/artifacts/frameworks/iOS-Simulator" \
+        -project "${ROOT_DIR}/MapboxMobileEvents.xcodeproj" \
         -scheme MapboxMobileEvents \
-        -destination="iOS Simulator" \
-        -sdk iphonesimulator \
-        -UseModernBuildSystem=YES \
-        -derivedDataPath ${ROOT_DIR}/build/derivedDataPath/iOS-Simulator \
-        ONLY_ACTIVE_ARCH=NO \
+        -destination "generic/platform=iOS Simulator" \
         BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-        SKIP_INSTALL=NO \
-        SUPPORTS_MACCATALYST=YES \
-        -configuration Release
+        SKIP_INSTALL=NO
 
 # build .xcframework
 xcodebuild \
     -create-xcframework \
     -framework "${ROOT_DIR}/build/artifacts/frameworks/iOS.xcarchive/Products/Library/Frameworks/MapboxMobileEvents.framework" \
+        -debug-symbols "${ROOT_DIR}/build/artifacts/frameworks/iOS.xcarchive/dSYMs/MapboxMobileEvents.framework.dSYM" \
     -framework "${ROOT_DIR}/build/artifacts/frameworks/iOS-Catalyst.xcarchive/Products/Library/Frameworks/MapboxMobileEvents.framework" \
+        -debug-symbols "${ROOT_DIR}/build/artifacts/frameworks/iOS-Catalyst.xcarchive/dSYMs/MapboxMobileEvents.framework.dSYM" \
     -framework "${ROOT_DIR}/build/artifacts/frameworks/iOS-Simulator.xcarchive/Products/Library/Frameworks/MapboxMobileEvents.framework" \
+        -debug-symbols "${ROOT_DIR}/build/artifacts/frameworks/iOS-Simulator.xcarchive/dSYMs/MapboxMobileEvents.framework.dSYM" \
     -output "${ROOT_DIR}/build/artifacts/frameworks/MapboxMobileEvents.xcframework"
 
 # zip artifacts
-mkdir -p ${ROOT_DIR}/build/artifacts/zip
-ZIPDIR=${ROOT_DIR}/build/artifacts/zip
+mkdir -p "${ROOT_DIR}/build/artifacts/zip"
+ZIPDIR="${ROOT_DIR}/build/artifacts/zip"
 
 pushd ${ROOT_DIR}/build/artifacts/frameworks/iOS.xcarchive/Products/Library/Frameworks
-zip -r $ZIPDIR/MapboxMobileEvents.zip MapboxMobileEvents.framework
+zip --symlinks -r "${ZIPDIR}/MapboxMobileEvents-ios.zip" MapboxMobileEvents.framework
 popd
 
 pushd ${ROOT_DIR}/build/artifacts/frameworks
-zip -r $ZIPDIR/MapboxMobileEvents.xcframework.zip MapboxMobileEvents.xcframework
+zip --symlinks -r "${ZIPDIR}/MapboxMobileEvents.zip" MapboxMobileEvents.xcframework
 popd
