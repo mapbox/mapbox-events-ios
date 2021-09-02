0686e344bd16dbe9a244fda32703f841b1d997f3
Vadim Yeshkunov <vadim.yeshkunov@mapbox.com>
2021-09-02 17:45:21 +0300

    Optimize useragent string builder
    Use in-memory list of loaded dylibs instead of
    [NSBundle allFrameworks] method to check all the
    loaded frameworks.

---
+++ Tests/Integration/ensure_netrc.sh
@@ -0,0 +1,14 @@
+HOMEDIR=~
+eval HOMEDIR=$HOMEDIR
+FILE="$HOMEDIR/.netrc"
+SDK_HOST="api.mapbox.com"
+
+if grep -q $SDK_HOST $FILE; then
+    echo "Entry for SDK Registry, not appending credentials."
+else
+    echo "machine api.mapbox.com" >> ~/.netrc
+    echo "login mapbox" >> ~/.netrc
+    echo "password ${MAPBOX_DOWNLOAD_TOKEN}" >> ~/.netrc
+    chmod 0600 ~/.netrc
+    echo "Entry added to netrc"
+fi
